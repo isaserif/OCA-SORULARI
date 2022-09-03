5aca313732adfbc6f4ec6374e1729ee2e009b577
isaserif <102700409+isaserif@users.noreply.github.com>
2022-09-03 14:35:25 +0300

    güncelleme

--- src/OCASorularİ/PostIncrementSorusu.java
+++ src/OCASorularİ/PostIncrementSorusu.java
@@ -16,6 +16,18 @@ public class PostIncrementSorusu {
                 System.out.println(num2+ " Hello word");
             }else {
                 System.out.println(num1+ "Hello universal");
+                /* postIncrement olduğu için önce num2 ye num1 değeri atanıyor.
+                num2=9  olur
+                ardından num1 e 1 arttırılıyor
+                num1=10 olur
+                num2 10 dan küçük olduğu için ; System.out.println(num2+ " Hello word");  çalışır.
+
+                eğer int num2=++num1; preIncrement olsaydı
+                num2=10 olur
+                num1=10 olur.
+                */
+
+
             }
 
     }
